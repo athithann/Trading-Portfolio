5a401e833b78a173968bed39ab025b42fbc4a9bc
athithann <athithan.navaneet@gmail.com>
2025-03-13 17:57:54 +0000

    Mar 13

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import 'react-web-tabs/dist/react-web-tabs.css';
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App profile='athithan' />} />
-      </Routes>
-    </BrowserRouter>
+    <App profile='athithan' />
   </React.StrictMode>,
   document.getElementById("root")
 );
